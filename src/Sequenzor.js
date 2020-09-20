bb863b9b1281059a4aaa0f006a6244df95fc626b
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2020-09-20 02:29:25 -0700

    Scaffold basic files
    Get React/Parcel up and going

---
+++ src/Sequenzor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sequenzor.css';
+
+function Sequenzor() {
+  return (
+    <div className="Sequenzor">
+        <h1>Sequenzor!</h1>
+    </div>
+  );
+}
+
+export default Sequenzor;
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Sequenzor from './Sequenzor';
+
+ReactDOM.render(<Sequenzor />, document.getElementById('root'));
