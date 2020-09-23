92fc2de06e4676b87882f081d1c55fdde4ab44ba
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2020-09-23 01:26:20 -0700

    Add styles
    Add styling to make the simulation appear like the classic toy the game is modeled after.

---
+++ src/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => (
+    <div className="Logo">
+        <span>Sequenzor</span>
+    </div>
+);
+
+export default Logo;
--- src/Sequenzor.js
+++ src/Sequenzor.js
@@ -3,6 +3,7 @@ import './Sequenzor.css';
 import SequenzorLogic from './SequenzorLogic';
 import SequenzorSynth from './SequenzorSynth.js';
 import Pad from './Pad.js';
+import Logo from './Logo.js';
 
 function Sequenzor() {
   const sequenzorSynth = SequenzorSynth();
@@ -29,14 +30,18 @@ function Sequenzor() {
 
   return (
     <div className="Sequenzor">
-        <h1>Sequenzor!</h1>
-        <p>Press any pad to begin</p>
-        <div>
-          <Pad onClick={events[0]} color="green" letter="q" />
-          <Pad onClick={events[1]} color="red" letter="w" />
-          <Pad onClick={events[2]} color="yellow" letter="a" />
-          <Pad onClick={events[3]} color="blue" letter="s" />
+        <h1>Sequenzor</h1>
+        <h3>It's the family-friendly memory game you know and love!</h3>
+        <div className="DeviceContainer">
+          <Logo />
+          <div className="PadContainer">
+            <Pad onClick={events[0]} color="green" letter="q" />
+            <Pad onClick={events[1]} color="red" letter="w" />
+            <Pad onClick={events[2]} color="yellow" letter="a" />
+            <Pad onClick={events[3]} color="blue" letter="s" />
+          </div>
         </div>
+        <p>Press any pad to begin.</p>
     </div>
   );
 }
